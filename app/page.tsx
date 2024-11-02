f0b9b2471aaa9307baad319d671f4af60da011ff
Vaughan <65584405+vaughanlove@users.noreply.github.com>
2024-11-02 06:08:51 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,43 @@
 "use client"; // Important: this ensures the wallet SDK only runs on the client
 
+import { useAuth } from "@crossmint/client-sdk-react-ui";
+
+function AuthButton() {
+  const { login, logout, jwt } = useAuth();
+
+  return (
+    <div>
+      {jwt == null ? (
+        <button 
+          type="button" 
+          onClick={login} 
+          className="bg-blue-500 text-white font-bold py-2 px-4 rounded"
+        >
+          Login
+        </button>
+      ) : (
+        <button 
+          type="button" 
+          onClick={logout} 
+          className="bg-black text-white font-bold py-2 px-4 rounded border-2 border-blue-500"
+        >
+          Logout
+        </button>
+      )}
+    </div>
+  );
+}
+
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="absolute top-0 right-0 p-4">
-        // TO-DO: we will add auth UI here
+        <AuthButton />
       </div>
       <div className="flex items-center justify-center w-full h-full">
-        // TO-DO: we will add wallet UI here
       </div>
     </main>
   );
 }
+
